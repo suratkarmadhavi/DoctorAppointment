a78048ae0582acd5b03b8e19c1667ca0eb001c3b
suratkarmadhavi456@gmail.com <suratkarmadhavi456@gmail.com>
2023-08-18 12:19:23 +0530

    doctor appointment updated

--- src/main/java/com/oneHealth/Appointments/OneHealthDoctorAppointmentApplication.java
+++ src/main/java/com/oneHealth/Appointments/OneHealthDoctorAppointmentApplication.java
@@ -3,8 +3,11 @@ package com.oneHealth.Appointments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class OneHealthDoctorAppointmentApplication {
 
 	public static void main(String[] args) {
